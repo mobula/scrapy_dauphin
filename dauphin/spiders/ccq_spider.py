c02c332ed3d2aca556241428e28424984e5437fa
Maze <maze@maze-photo.com>
2016-06-12 20:20:10 -0400

    ccq spider_update

--- dauphin/spiders/ccq_spider.py
+++ dauphin/spiders/ccq_spider.py
@@ -1,21 +1,23 @@
+# -*- coding: utf-8 -*-
 import scrapy
 
 from dauphin.items import RssItem
 
-class DmozSpider(scrapy.Spider):
+
+class CcqSpider(scrapy.Spider):
     name = "ccq"
     allowed_domains = ["ccq.org"]
     start_urls = [
         "http://www.ccq.org/fr-CA/nouvelles?profil=GrandPublic"
     ]
+    root_url = ["http://www.ccq.org"]
 
     def parse(self, response):
-        rootURL = ["http://www.ccq.org"]
         for sel in response.css('.news'):
             item = RssItem()
             item['title'] = sel.xpath('h3/a/text()').extract()
-            link = [''.join(rootURL+sel.xpath('h3/a/@href').extract())]
+            link = [''.join(self.rootURL+sel.xpath('h3/a/@href').extract())]
             item['link'] = link
             item['date'] = sel.css('.nouvellesDate').xpath('text()').extract()
-            print 'ITEM: ', item
             yield item
+
