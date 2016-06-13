3535568109a62715727d899fc2dde012452677e1
Maze <maze@maze-photo.com>
2016-06-12 20:03:34 -0400

    ccq spider

--- dauphin/spiders/ccq_spider.py
+++ dauphin/spiders/ccq_spider.py
@@ -1,40 +1,21 @@
-# -*- coding: utf-8 -*-
-# from scrapy.spiders import XMLFeedSpider
 import scrapy
 
 from dauphin.items import RssItem
 
-
-class CcqSpider(scrapy.Spider):
-    name = 'ccq'
-    allowed_domains = ['ccq.org']
-    start_urls = ['https://www.ccq.org/fr-CA/Nouvelles']
-    # iterator = 'iternodes' # you can change this; see the docs
-    # itertag = 'item' # change it accordingly
-
-    # def parse_node(self, response, selector):
-    #     i = DauphinItem()
-    #     #i['url'] = selector.select('url').extract()
-    #     #i['name'] = selector.select('name').extract()
-    #     #i['description'] = selector.select('description').extract()
-    #     return i
+class DmozSpider(scrapy.Spider):
+    name = "ccq"
+    allowed_domains = ["ccq.org"]
+    start_urls = [
+        "http://www.ccq.org/fr-CA/nouvelles?profil=GrandPublic"
+    ]
 
     def parse(self, response):
-        # filename = response.url.split("/")[-2] + '.html'
-        # page_title = response.xpath('//title/text()').extract()[0]
-        # print page_title
-
-        # articleSelector = response.xpath('//article')
-        # timeSelector = response.xpath('//article//time/@datetime').extract()
-        # titleSelector = response.xpath('//article//h2').extract()
-        # linkSelector = response.xpath('//article//h2/a/@href').extract()
-
-        for sel in response.xpath('//article'):
-            title = sel.xpath('//h2/a/text()').extract()
-            # link = sel.xpath('//h2/a/@href').extract()
-            # desc = sel.xpath('text()').extract()
-            # date_time = sel.xpath('//time/@datetime')
-            print title #,  link, date_time
-
-        # with open(filename, 'wb') as f:
-        #     f.write(response.body)
+        rootURL = ["http://www.ccq.org"]
+        for sel in response.css('.news'):
+            item = RssItem()
+            item['title'] = sel.xpath('h3/a/text()').extract()
+            link = [''.join(rootURL+sel.xpath('h3/a/@href').extract())]
+            item['link'] = link
+            item['date'] = sel.css('.nouvellesDate').xpath('text()').extract()
+            print 'ITEM: ', item
+            yield item
