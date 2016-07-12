5e99a6e9e6a9fa49983889a6be3cc5427714a744
Sophie Leroy <mobula.diabolus@gmail.com>
2016-07-12 00:04:44 -0400

    Fix output dates on UPA

--- dauphin/spiders/upa_spider.py
+++ dauphin/spiders/upa_spider.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-# from scrapy.spiders import XMLFeedSpider
+import datetime
+import locale
+
 import scrapy
 
 from dauphin.items import RssItem
@@ -9,22 +11,33 @@ class UpaSpider(scrapy.Spider):
     name = 'upa'
     allowed_domains = ['upa.qc.ca']
     start_urls = ['http://www.upa.qc.ca/fr/communiques/']
-    title = 'Communications UPA'
+    title = "Communications UPA"
+    description = u"Communiqués de l'Union des Producteurs Agrigoles du Québec"
+    in_datetime_fmt = '%Y-%m-%dT%H:%M:%S+00:00'
+    rss_datetime_fmt = '%a, %d %b %Y %H:%M:%S +0000'
 
     def parse(self, response):
 
         for sel in response.xpath('//article[@class="featured-box__wrapper cf"]'):
             item = RssItem()
             item['title'] = sel.xpath('h1/text()')[0].extract()
-            item['pubDate'] = sel.xpath('p[@class="featured-box__meta"]/date/@datetime')[0].extract()
+            item['pubDate'] =  self._to_rss_date(sel.xpath('p[@class="featured-box__meta"]/date/@datetime')[0].extract())
             item['link'] = sel.xpath('p[@class="featured-box__cta"]/a/@href')[0].extract()
+            # TODO: guid
             item['description'] = sel.xpath('div[@class="featured-box__content"]/p/text()')[0].extract()
             yield item
 
         for sel in response.xpath('//article/header'):
             item = RssItem()
             item['title'] = sel.xpath('h2/a/text()')[0].extract()
-            item['pubDate'] = sel.xpath('p/time/@datetime')[0].extract()
+            item['pubDate'] = self._to_rss_date(sel.xpath('p/time/@datetime')[0].extract())
             item['link'] = sel.xpath('h2/a/@href')[0].extract()
             yield item
 
+    def _to_rss_date(self, datestr):
+        loc = locale.getlocale() # store current locale
+        d = datetime.datetime.strptime(datestr, self.in_datetime_fmt)
+        locale.setlocale(locale.LC_ALL, 'C')
+        output = d.strftime(self.rss_datetime_fmt)
+        locale.setlocale(locale.LC_ALL, loc)
+        return output
