344d0c7bbfddd05c5f40a931d77d3a6a01c30e80
Sophie Leroy <mobula.diabolus@gmail.com>
2016-07-16 14:02:39 -0400

    Mern. Extract description in sub pages
    Aussi:
    - Fix sur le parsing des dates (encodage)
    - pour les catégories, xpath siblings selector ne retournait pas les résultats
    J'ai essayé d'extraire les secteurs et de parser les pages filtrées, mais la page de communiqués "Le ministère" retourne tous les résultats. http://mern.gouv.qc.ca/presse/communiques.jsp?idSecteur=112
    Donc retour à la case départ. Pour le moment le feed ne retourne pas de catégories.

--- dauphin/spiders/mern_spider.py
+++ dauphin/spiders/mern_spider.py
@@ -3,7 +3,7 @@ import datetime
 import locale
 
 import scrapy
-from scrapy.linkextractors import LinkExtractor
+from scrapy.utils.url import canonicalize_url
 
 from dauphin.items import RssItem
 
@@ -17,8 +17,43 @@ class MernSpider(scrapy.Spider):
     description = u"Communiqués du MERN"
     in_datetime_fmt = '%Y-%m-%dT%H:%M:%S+00:00'
     rss_datetime_fmt = '%a, %d %b %Y %H:%M:%S +0000'
+    exclude_sectors = [ 112 ] # Le Ministère = ne respecte pas le filtrage
 
     def parse(self, response):
+        # return self.parse_secteurs(response)
+        return self.parse_communiques(response)
+
+    def parse_secteurs(self, response):
+        """Récupère les différents secteurs et va chercher les pages dédiées.
+
+        Il y a un problème avec la sous-page "Le ministère", qui retourne toutes les catégories
+        """
+        selectors = response.xpath('//select[@id="idSecteur"]/option')
+        # secteurs = { sel.xpath('@value')[0].extract(): sel.xpath('text()')[0].extract() for sel in selectors }
+        for sel in selectors:
+            secteur_id = sel.xpath('@value')[0].extract()
+            if int(secteur_id) not in self.exclude_sectors:
+                secteur = sel.xpath('text()')[0].extract()
+                url = response.url + '?idSecteur=' + secteur_id
+                yield scrapy.Request(url=url, callback=self.parse_communiques, meta={ 'category': secteur })
+
+    def parse_communiques(self, response):
+        category = response.meta.get('category', None)
+
+        communiques = response.css('p.communiques')
+        for communique in communiques:
+            item = RssItem()
+            if category:
+                item['category'] = category
+            item['pubDate'] = self._to_rss_date(communique.xpath('b/text()').re_first('(.*)\s.*'))
+            item['title'] = communique.xpath('a/text()')[0].extract()
+            link = self._get_uri(communique.xpath('a//@href')[0].extract())
+            item['link'] = link
+            item['guid'] = canonicalize_url(link)
+            yield scrapy.Request(link, callback=self.parse_detail, meta={ 'item': item })
+
+    def parse_by_category(self, response):
+        """ Tentative de trouver les communiqués reliés à une catégorie particulière """
         for cat in response.xpath('//p[@class="ss-titre"]'):
             category = cat.xpath('text()')[0].extract()
             communiques = cat.xpath('following-sibling::*').css('p.communiques')
@@ -27,29 +62,25 @@ class MernSpider(scrapy.Spider):
                 item['category'] = category
                 item['pubDate'] = self._to_rss_date(communique.xpath('b/text()').re_first('(.*)\s.*'))
                 item['title'] = communique.xpath('a/text()')[0].extract()
-                item['link'] = self._get_uri(communique.xpath('a//@href')[0].extract())
-                # content_page = self._link_content(link)
-                # item['description'] = content_page.css('span.contenu')[0].extract()
-                yield item
+                link = self._get_uri(communique.xpath('a//@href')[0].extract())
+                item['link'] = link
+                item['guid'] = canonicalize_url(link)
+                yield scrapy.Request(url=link, callback=self.parse_detail, meta={ 'item': item })
+
+    def parse_detail(self, response):
+        item = response.meta.get('item', None)
+        contenu = response.xpath('string(//p[@class="contenu"])')[0].extract()
+        item['description'] = contenu[:2000] + "..."
+        return item
 
     def _get_uri(self, url):
         return self.root_url + url
 
-    def _link_content(self, link):
-        pass
-
     def _to_rss_date(self, datestr):
         loc = locale.getlocale() # store current locale
-        locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
-        d = datetime.datetime.strptime(datestr, '%d %B %Y')
-        locale.setlocale(locale.LC_ALL, 'C')
+        locale.setlocale(locale.LC_TIME, 'fr_FR.UTF-8')
+        d = datetime.datetime.strptime(datestr.encode('utf-8'), '%d %B %Y')
+        locale.setlocale(locale.LC_TIME, 'C')
         output = d.strftime(self.rss_datetime_fmt)
-        locale.setlocale(locale.LC_ALL, loc)
+        locale.setlocale(locale.LC_TIME, loc)
         return output
-
-    # def parse_node(self, response, selector):
-    #     i = DauphinItem()
-    #     #i['url'] = selector.select('url').extract()
-    #     #i['name'] = selector.select('name').extract()
-    #     #i['description'] = selector.select('description').extract()
-    #     return i
